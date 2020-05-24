5155feb3c90fcb0106e08a52a2a58069a781b3b8
千葉比呂眸 <chibahiromu@chibahirohitomi-no-MacBook-Air.local>
2020-05-24 09:06:37 +0900

    init

---
+++ app/javascript/app.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app id="app"> <!--divから変更-->
+    <Header/> <!--追記-->
+    <h1>This is app.vue</h1> <!--追記-->
+    <p>{{ message }}</p>
+    <Top/> <!--追記-->
+  </v-app>
+</template>
+
+<script>
+import Header from "./components/Header"; //追記
+import Top from "./components/Top"; //追記
+
+export default {
+  data: function () {
+    return {
+      message: "Hello Vue!"
+    }
+  },
+  components: {
+    Header, //追記
+    Top, //追記
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
